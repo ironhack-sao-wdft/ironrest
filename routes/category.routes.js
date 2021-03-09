f732b9f7b34d1b44f84dc7cc30f80a7060c661d3
leonardbwkh <leonard_bwkh@hotmail.com>
2021-03-08 22:27:50 -0300

    continued working on categories

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -2,7 +2,13 @@ const express = require("express");
 const Category = require("../models/Category");
 const slugify = require("slugify");
 const router = express.Router();
+const {
+  requireSignin,
+  adminMiddleware,
+} = require("../common-middleware/index");
 
+// Crud - Create
+// router.post("/category/create", requireSignin, adminMiddleware, (req, res) => {
 router.post("/category/create", (req, res) => {
   const categoryObj = {
     name: req.body.name,
@@ -15,6 +21,18 @@ router.post("/category/create", (req, res) => {
       return res.status(201).json({ category });
     }
   });
+});
+
+// cRud - Read
+router.get("/category/get", async (req, res) => {
+  try {
+    console.log("ei ta funfando");
+    const categories = await Category.find();
+    console.log(categories);
+    return res.status(200).json(categories);
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
 });
 
 module.exports = router;
