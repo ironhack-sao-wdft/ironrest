d64062d03256df4788a9e636a97680ead1637f2a
Felipe Freitas <afelipe.freitas@gmail.com>
2021-03-11 12:49:31 -0300

    Transaction Collection Created

--- models/User.js
+++ models/User.js
@@ -1,12 +1,15 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Mongoose } = require('mongoose');
 
 const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  passwordHash: { type: String, required: true },
-  role: {type: String, default: "customer"}
+	email: { type: String, required: true, unique: true },
+	name: { type: String, required: true },
+	passwordHash: { type: String, required: true },
+	role: { type: String, default: 'customer' },
+	transactions: [
+		{ type: Schema.Types.ObjectId, ref: 'Transaction', required: false },
+	],
 });
 
-const UserModel = model("User", UserSchema);
+const UserModel = model('User', UserSchema);
 
 module.exports = UserModel;
