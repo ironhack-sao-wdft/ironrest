26d61e73c5b9280e35516cc66e514556957cdcd3
Felipe Freitas <afelipe.freitas@gmail.com>
2021-03-12 11:00:08 -0300

    done

--- routes/transactions.routes.js
+++ routes/transactions.routes.js
@@ -12,9 +12,8 @@ router.post('/create-checkout-session', async (req, res) => {
 		payment_method_types: ['card'],
 		line_items: req.body.products,
 		mode: 'payment',
-		success_url:
-			'https://oddmarket.netlify.app/order/sucess/{CHECKOUT_SESSION_ID}',
-		cancel_url: 'https://oddmarket.netlify.app/order/canceled',
+		success_url: 'hhttp://localhost:3000//order/sucess/{CHECKOUT_SESSION_ID}',
+		cancel_url: 'http://localhost:3000//order/canceled',
 		client_reference_id: req.body.id,
 	});
 
