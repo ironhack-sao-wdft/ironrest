5b5dd58da55cf69aff90b73fc3fb0f82b832be73
Igor Stefano <igstefano90@live.com>
2021-12-11 12:27:15 -0300

    add activity model

--- models/User.model.js
+++ models/User.model.js
@@ -19,6 +19,7 @@ const UserSchema = new Schema({
   favorites: [{ type: String }],
   blockedActivities: [{ type: String }],
   publishedActivities: [{ type: mongoose.Types.ObjectId, ref: "Activity" }],
+  pictureURL: { type: String, trim: true },
 });
 
 const UserModel = model("User", UserSchema);
