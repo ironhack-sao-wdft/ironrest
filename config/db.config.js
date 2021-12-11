0d4f5f832d67ce4bf72af1c326296df043eb9b48
William Galvao <wsg.online@gmail.com>
2021-12-11 14:29:03 -0300

    config db fixed

--- config/db.config.js
+++ config/db.config.js
@@ -4,10 +4,8 @@ async function connect() {
   try {
     // Não esquecer de criar variável de ambiente com endereço do seu servidor Mongo local em desenvolvimento, e o endereço do cluster do Atlas em produção
     const connection = await mongoose.connect(process.env.MONGODB_URI, {
-      useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
     });
     console.log("Connected to DB: ", connection.connection.name);
   } catch (err) {
