e09fb68e0a7ade034baeb1db70493868a01c6834
William Galvao <wsg.online@gmail.com>
2021-12-14 07:49:45 -0300

    minor fix

--- config/db.config.js
+++ config/db.config.js
@@ -3,16 +3,11 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     // Não esquecer de criar variável de ambiente com endereço do seu servidor Mongo local em desenvolvimento, e o endereço do cluster do Atlas em produção
-<<<<<<< HEAD
     const connection = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("Connected to DB: ", connection.connection.name);
-=======
-    const connection = await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Conectado ao banco de dados", connection.connection.name);
->>>>>>> be0acca55837dbfc58d9e8ad405ec77cd4daa778
   } catch (err) {
     console.error("Erro de conexão ao banco de dados:", err);
   }
