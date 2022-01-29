15d0af3216e71f4bd859407c6418ddd049352b91
Victoria Miranda <vitorianevesmiranda@gmail.com>
2022-01-29 13:18:57 -0300

    teste

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 // Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://bookregister.netlify.app" }));
 
 const userRouter = require("./routes/user.routes");
 app.use("/api", userRouter);
